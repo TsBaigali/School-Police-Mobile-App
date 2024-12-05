332a8dcd51b8962595823a6d6a588d3f21abcf56
Baigali <baigalitsts@gmail.com>
2024-12-06 01:29:55 +0800

    Add Post hiihdee duurgiin daguu surguuli garah

---
+++ android/lib/models/school.dart
@@ -0,0 +1,7 @@
+class School {
+  final String name;
+  final double latitude;
+  final double longitude;
+
+  School({required this.name, required this.latitude, required this.longitude});
+}
--- android/lib/screens/add_post_screen/add_post_state.dart
+++ android/lib/screens/add_post_screen/add_post_state.dart
@@ -3,25 +3,45 @@ import '../../models/ad.dart';
 abstract class AddPostState {
   final String? selectedDistrict;
   final String? selectedShift;
+  final String? selectedSchool;
 
-  AddPostState({this.selectedDistrict, this.selectedShift});
+  // Dynamic list of school options
+  final List<String> availableSchools;
+
+  AddPostState({
+    this.selectedDistrict,
+    this.selectedShift,
+    this.selectedSchool,
+    required this.availableSchools,
+  });
 }
 
 class AddPostInitial extends AddPostState {
-  AddPostInitial({String? selectedDistrict, String? selectedShift})
-      : super(selectedDistrict: selectedDistrict, selectedShift: selectedShift);
+  AddPostInitial({
+    String? selectedDistrict,
+    String? selectedShift,
+    String? selectedSchool,
+    required List<String> availableSchools,
+  }) : super(
+    selectedDistrict: selectedDistrict,
+    selectedShift: selectedShift,
+    selectedSchool: selectedSchool,
+    availableSchools: availableSchools,
+  );
 }
 
-class AddPostLoading extends AddPostState {}
+class AddPostLoading extends AddPostState {
+  AddPostLoading() : super(availableSchools: []);
+}
 
 class AddPostSuccess extends AddPostState {
   final Ad ad;
 
-  AddPostSuccess(this.ad);
+  AddPostSuccess(this.ad) : super(availableSchools: []);
 }
 
 class AddPostFailure extends AddPostState {
   final String error;
 
-  AddPostFailure({required this.error});
+  AddPostFailure({required this.error}) : super(availableSchools: []);
 }
